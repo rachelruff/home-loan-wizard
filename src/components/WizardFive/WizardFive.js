855a24f984a4ce3f29c481d38bc650debb5238a8
rachelruff <33016102+rachelruff@users.noreply.github.com>
2017-12-19 10:15:30 -0600

    Last numbeer is left

--- src/components/WizardFive/WizardFive.js
+++ src/components/WizardFive/WizardFive.js
@@ -1,22 +1,33 @@
-import React,  { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { updateRealEstateAgent } from "../../ducks/reducer";
 
 class WizardFive extends Component {
-
-    render(){
-        return(
-            <div className="parent-div">
-                    <div className="vert-align">    
-
-                    <p>Are you currently working with a real estate agent?</p> <br />
-                    <div className="row">
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentTrue}>Yes</button></Link>
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentFalse}>No </button></Link>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { updateRealEstateAgent } = this.props;
+    return (
+      <div className="parent-div">
+        <div className="vert-align">
+          <p>Are you currently working with a real estate agent?</p> <br />
+          <div className="row">
+            <Link to="/wSix">
+              <button onClick={e => updateRealEstateAgent(true)}>Yes</button>
+            </Link>
+            <Link to="/wSix">
+              <button onClick={e => updateRealEstateAgent(false)}>No </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+function mapStateToProps(state) {
+  const { realEstateAgent } = state;
 
-export default WizardFive;
+  return {
+    realEstateAgent
+  };
+}
+export default connect(mapStateToProps, { updateRealEstateAgent })(WizardFive);
